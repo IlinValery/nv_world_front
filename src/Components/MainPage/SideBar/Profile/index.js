7713af03f276ef82eccc4f7eaa17a2b5f507929d
Polina <polina.ponomareva@skoltech.ru>
2020-10-04 06:21:20 +0300

    Implemented the person modal page with editing mail and password

--- src/Components/MainPage/SideBar/Profile/index.js
+++ src/Components/MainPage/SideBar/Profile/index.js
@@ -4,6 +4,7 @@ import Photo from "./Photo"
 import Name from "./Name"
 import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import SkillList from "./Description/Skills_list"
+import ModalPerson from "../ModalPerson";
 
 export default class Profile extends Component {
     constructor(props) {
@@ -18,6 +19,9 @@ export default class Profile extends Component {
         return (
             <Container fluid={true}>
                 <Row>
+                    <Col xl={1}>
+                        <ModalPerson/>
+                    </Col>
                     <Col>
                         <div className={s.whole_page}>
                             {/*{this.props.children}*/}
